a56eac550674f8eb9805594ea051385ff46c685e
Kourani <76729542m@gmail.com>
2023-04-12 20:31:42 -0400

    minor changes to code layout

--- backEnd/routes/api/index.js
+++ backEnd/routes/api/index.js
@@ -1,23 +1,16 @@
-// backend/routes/api/index.js
+
+
 const router = require('express').Router();
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
 
-
-// backend/routes/api/index.js
-// ...
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
-  // ...
-
-
-  // backend/routes/api/index.js
-// ...
-
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
     where: {
@@ -28,17 +21,6 @@ router.get('/set-token-cookie', async (_req, res) => {
   return res.json({ user: user });
 });
 
-// ...
-
-
-// backend/routes/api/index.js
-// ...
-
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
-
-router.use(restoreUser);
-
 router.get(
   '/restore-user',
   (req, res) => {
@@ -46,17 +28,6 @@ router.get(
   }
 );
 
-// ...
-
-// backend/routes/api/index.js
-// ...
-
-router.use(restoreUser);
-
-// ...
-
-// GET /api/require-auth
-const { requireAuth } = require('../../utils/auth.js');
 router.get(
   '/require-auth',
   requireAuth,
@@ -65,11 +36,6 @@ router.get(
   }
 );
 
-// ...
-
-
-
-
-
+router.use(restoreUser);
 
 module.exports = router;
