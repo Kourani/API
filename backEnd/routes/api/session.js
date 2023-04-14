ac5c1bf314086e3f0eabd5a7884742131fd35798
Kourani <76729542m@gmail.com>
2023-04-13 20:37:46 -0400

    Add a backend endpoint to get the current user session

--- backEnd/routes/api/session.js
+++ backEnd/routes/api/session.js
@@ -51,4 +51,30 @@ router.post(
     }
   );
 
+  // backend/routes/api/session.js
+// ...
+
+
+
+
+
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
+
     module.exports = router;
