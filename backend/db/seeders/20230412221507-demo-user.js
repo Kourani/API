3893d61b0a02e9103c4622cc49ea66e2ec354286
Kourani <76729542m@gmail.com>
2023-04-15 15:48:01 -0400

    bug fix

--- backend/db/seeders/20230412221507-demo-user.js
+++ backend/db/seeders/20230412221507-demo-user.js
@@ -33,11 +33,15 @@ module.exports = {
     hashedPassword:bcrypt.hashSync('password')
     },
     {
+      firstName: 'Lost',
+      lastName: 'Avenger',
       email: 'user1@user.io',
       username: 'FakeUser1',
       hashedPassword: bcrypt.hashSync('password2')
     },
     {
+      firstName: 'Lost',
+      lastName: 'Avenger',
       email: 'user2@user.io',
       username: 'FakeUser2',
       hashedPassword: bcrypt.hashSync('password3')
