45d9aff051fae5b4be5e5ed636fc4440bb969d79
Kourani <76729542m@gmail.com>
2023-04-18 20:43:52 -0400

    auth and deploy complete start kanban cards

---
+++ backend/routes/index.js
@@ -0,0 +1,20 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+const apiRouter = require('./api/index')
+
+router.get('/api/csrf/restore', function(req, res) {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+router.use('/api', apiRouter);
+
+
+
+
+module.exports = router;
